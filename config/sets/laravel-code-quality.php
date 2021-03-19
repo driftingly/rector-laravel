8f9bb2f6f39825055c6034a91f56943a801a84bd
TomasVotruba <tomas.vot@gmail.com>
2021-03-19 01:31:54 +0100

    add set paths

--- config/sets/laravel-code-quality.php
+++ config/sets/laravel-code-quality.php
@@ -2,17 +2,11 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Laravel\Rector\Assign\CallOnAppArrayAccessToStandaloneAssignRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
-    $services->defaults()
-        ->public()
-        ->autowire()
-        ->autoconfigure();
-
-    $services->load('Rector\\', __DIR__ . '/../src')
-        ->exclude([__DIR__ . '/../src/{Rector,ValueObject}']);
+    $services->set(CallOnAppArrayAccessToStandaloneAssignRector::class);
 };
