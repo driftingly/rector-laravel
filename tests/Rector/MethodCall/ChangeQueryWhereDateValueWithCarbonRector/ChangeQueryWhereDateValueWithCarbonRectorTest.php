8257009344a5a1d4637f85b48dc00c362cf8f8b2
TomasVotruba <tomas.vot@gmail.com>
2021-03-20 11:30:14 +0100

    use public method

--- tests/Rector/MethodCall/ChangeQueryWhereDateValueWithCarbonRector/ChangeQueryWhereDateValueWithCarbonRectorTest.php
+++ tests/Rector/MethodCall/ChangeQueryWhereDateValueWithCarbonRector/ChangeQueryWhereDateValueWithCarbonRectorTest.php
@@ -26,7 +26,7 @@ final class ChangeQueryWhereDateValueWithCarbonRectorTest extends AbstractRector
         return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
     }
 
-    protected function provideConfigFilePath(): string
+    public function provideConfigFilePath(): string
     {
         return __DIR__ . '/config/configured_rule.php';
     }
--- tests/Rector/StaticCall/Redirect301ToPermanentRedirectRector/Redirect301ToPermanentRedirectRectorTest.php
+++ tests/Rector/StaticCall/Redirect301ToPermanentRedirectRector/Redirect301ToPermanentRedirectRectorTest.php
@@ -26,7 +26,7 @@ final class Redirect301ToPermanentRedirectRectorTest extends AbstractRectorTestC
         return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
     }
 
-    protected function provideConfigFilePath(): string
+    public function provideConfigFilePath(): string
     {
         return __DIR__ . '/config/configured_rule.php';
     }
