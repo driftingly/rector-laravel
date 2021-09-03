92dacc63a1e2f1b075a96fbba28557ad9120399e
zingimmick <zingimmick@outlook.com>
2021-09-03 16:12:10 +0800

    Add set for Laravel 7.x

--- src/Set/LaravelSetList.php
+++ src/Set/LaravelSetList.php
@@ -66,6 +66,11 @@ final class LaravelSetList implements SetListInterface
     /**
      * @var string
      */
+    public const LARAVEL_70 = __DIR__ . '/../../config/sets/laravel70.php';
+
+    /**
+     * @var string
+     */
     public const LARAVEL_STATIC_TO_INJECTION = __DIR__ . '/../../config/sets/laravel-static-to-injection.php';
 
     /**
