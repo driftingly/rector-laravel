20f7722f5812de1bba8f64ac6c885db27381b5e2
zingimmick <zingimmick@outlook.com>
2021-09-13 12:38:42 +0800

    Fix cs

--- stubs/Illuminate/Support/Facades/Route.php
+++ stubs/Illuminate/Support/Facades/Route.php
@@ -1,9 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Illuminate\Support\Facades;
+
 if (class_exists('\Illuminate\Support\Facades\Route')) {
     return;
 }
+
 /**
  * @method static \Illuminate\Routing\RouteRegistrar middleware(array|string|null $middleware)
  */
