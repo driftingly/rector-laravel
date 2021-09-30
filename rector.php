1dae5c81ab046bc44d1923df0628e5f4f5466a74
Tomas Votruba <tomas.vot@gmail.com>
2021-09-30 16:00:24 +0200

    [CI] Add Rector (#18)
    * setup Rector
    
    * ready

--- rector.php
+++ rector.php
@@ -2,13 +2,42 @@
 
 declare(strict_types=1);
 
-use Rector\Php74\Rector\Property\TypedPropertyRector;
-use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Core\Configuration\Option;
+use Rector\Nette\NodeAnalyzer\BinaryOpAnalyzer;
+use Rector\PHPUnit\NodeAnalyzer\TestsNodeAnalyzer;
+use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
+    $parameters = $containerConfigurator->parameters();
 
-    $services->set(TypedPropertyRector::class);
-    $services->set(ClassPropertyAssignToConstructorPromotionRector::class);
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+    $parameters->set(Option::SKIP, [
+        // for tests
+        '*/Source/*',
+        '*/Fixture/*',
+    ]);
+
+    // needed for DEAD_CODE list, just in split package like this
+    $containerConfigurator->import(__DIR__ . '/config/config.php');
+
+    // reuqired for PHP 8
+    $services = $containerConfigurator->services();
+    $services->defaults()
+        ->public()
+        ->autoconfigure()
+        ->autowire();
+
+    // needed for sets bellow, only for this split package
+    $services->set(BinaryOpAnalyzer::class);
+    $services->set(TestsNodeAnalyzer::class);
+
+    $containerConfigurator->import(SetList::PHP_80);
+    $containerConfigurator->import(SetList::PHP_74);
+    $containerConfigurator->import(SetList::PHP_73);
+    $containerConfigurator->import(SetList::DEAD_CODE);
 };
