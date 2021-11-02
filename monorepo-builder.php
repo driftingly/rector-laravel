3b0130134b8ddda3d21f861b0b20ff3a5ca3dbab
Tomas Votruba <tomas.vot@gmail.com>
2021-11-02 23:44:46 +0100

    add monorepo builder

---
+++ monorepo-builder.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->defaults()
+        ->autowire();
+
+    // @see https://github.com/symplify/monorepo-builder#6-release-flow
+    $services->set(TagVersionReleaseWorker::class);
+    $services->set(PushTagReleaseWorker::class);
+};
