648ef9d6cb1115ac8e2d5b0430bb93f028915f6c
Tomas Votruba <tomas.vot@gmail.com>
2021-11-29 02:07:59 +0300

    add code quality set

--- rector.php
+++ rector.php
@@ -30,4 +30,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
     $containerConfigurator->import(SetList::DEAD_CODE);
+    $containerConfigurator->import(SetList::CODE_QUALITY);
 };
