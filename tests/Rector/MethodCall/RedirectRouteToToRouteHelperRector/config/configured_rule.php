e632d488739d96861cc4a6e8c27eb83834fcd59c
Hiren Keraliya <hiren@freshbits.in>
2022-05-14 19:01:51 +0530

    Add New Refactore Rule - Redirect Route To To Route Helper (#45)
    * Add tests
    
    * Add new rector for the redirect route to to route helper call
    
    * Add documentation
    
    * Remove unnecessary code.

---
+++ tests/Rector/MethodCall/RedirectRouteToToRouteHelperRector/config/configured_rule.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+use Rector\Laravel\Rector\MethodCall\RedirectRouteToToRouteHelperRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+
+    $rectorConfig->rule(RedirectRouteToToRouteHelperRector::class);
+};
