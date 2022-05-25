775f8c6e6cc562d61e17e3025c50320b75966d8a
Hiren <hiren@freshbits.in>
2022-05-25 20:04:02 +0530

    Add more upgrade

--- config/sets/laravel90.php
+++ config/sets/laravel90.php
@@ -42,6 +42,39 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig
+        ->ruleWithConfiguration(ArgumentAdderRector::class, [new ArgumentAdder(
+            'Illuminate\Database\Eloquent\Model',
+            'touch',
+            0,
+            'attribute',
+        ),
+    ]);
+
+    $rectorConfig
+        ->ruleWithConfiguration(ArgumentAdderRector::class, [new ArgumentAdder(
+            'Illuminate\Queue\Failed\FailedJobProviderInterface',
+            'flush',
+            0,
+            'hours',
+        ),
+    ]);
+
+    $rectorConfig
+        ->ruleWithConfiguration(ArgumentAdderRector::class, [new ArgumentAdder(
+            'Illuminate\Foundation\Http\FormRequest',
+            'validated',
+            0,
+            'key',
+        ),
+        new ArgumentAdder(
+            'Illuminate\Foundation\Http\FormRequest',
+            'validated',
+            1,
+            'default',
+        )
+    ]);
+
+    $rectorConfig
         ->ruleWithConfiguration(ChangeMethodVisibilityRector::class, [new ChangeMethodVisibility(
             'Illuminate\Contracts\Foundation\Application',
             'ignore',
@@ -49,11 +82,24 @@ return static function (RectorConfig $rectorConfig): void {
         ),
     ]);
 
-
     $rectorConfig
         ->ruleWithConfiguration(RenameMethodRector::class, [
             new MethodCallRename('Illuminate\Support\Enumerable', 'reduceWithKeys', 'reduce'),
 
             new MethodCallRename('Illuminate\Support\Enumerable', 'reduceMany', 'reduceSpread'),
+
+            new MethodCallRename('Illuminate\Mail\Message', 'getSwiftMessage', 'getSymfonyMessage'),
+
+            new MethodCallRename('Illuminate\Mail\Mailable', 'withSwiftMessage', 'withSymfonyMessage'),
+
+            new MethodCallRename('Illuminate\Notifications\Messages\MailMessage', 'withSwiftMessage', 'withSymfonyMessage'),
+
+            new MethodCallRename('Illuminate\Mail\Mailer', 'getSwiftMailer', 'getSymfonyTransport'),
+
+            new MethodCallRename('Illuminate\Mail\Mailer', 'setSwiftMailer', 'setSymfonyTransport'),
+
+            new MethodCallRename('Illuminate\Mail\MailManager', 'createTransport', 'createSymfonyTransport'),
+
+            new MethodCallRename('Illuminate\Testing\TestResponse', 'assertDeleted', 'assertModelMissing'),
         ]);
 };
