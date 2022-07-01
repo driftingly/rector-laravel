5464b4377eb52e8ed0d99c0b65577b36c6f8c1e7
simivar <simivar@gmail.com>
2022-07-01 16:19:14 +0200

    Fix autowiring

--- tests/Rector/MethodCall/LumenRoutesStringMiddlewareToArrayRector/config/configured_rule.php
+++ tests/Rector/MethodCall/LumenRoutesStringMiddlewareToArrayRector/config/configured_rule.php
@@ -6,5 +6,7 @@ use Rector\Config\RectorConfig;
 use Rector\Laravel\Rector\MethodCall\LumenRoutesStringMiddlewareToArrayRector;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+
     $rectorConfig->rule(LumenRoutesStringMiddlewareToArrayRector::class);
 };
