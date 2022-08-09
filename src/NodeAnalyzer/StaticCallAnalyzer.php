20edb95fcd91239389270534100b2eb9c39cdacc
Tomas Votruba <tomas.vot@gmail.com>
2022-08-09 22:07:01 +0200

    cs

--- src/NodeAnalyzer/StaticCallAnalyzer.php
+++ src/NodeAnalyzer/StaticCallAnalyzer.php
@@ -12,11 +12,7 @@ use Rector\NodeNameResolver\NodeNameResolver;
 final class StaticCallAnalyzer
 {
     public function __construct(
-<<<<<<< HEAD
         private readonly NodeNameResolver $nodeNameResolver
-=======
-        private NodeNameResolver $nodeNameResolver
->>>>>>> add own static call analyzer
     ) {
     }
 
