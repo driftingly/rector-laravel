20ae0bd0e99a12f42ff997098ee8d8b884b195d8
geni_jaho <jahogeni@gmail.com>
2023-04-11 22:05:51 +0200

    Add Laravel Cashier 13 upgrade rules

---
+++ src/Rector/Class_/CashierStripeOptionsToStripeRector.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RectorLaravel\Rector\Class_;
+
+use PhpParser\Node;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Type\ObjectType;
+use Rector\Core\Rector\AbstractScopeAwareRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://github.com/laravel/cashier-stripe/blob/master/UPGRADE.md#upgrading-to-130-from-12x
+ *
+ * @see \RectorLaravel\Tests\Rector\Class_\CashierStripeOptionsToStripeRector\CashierStripeOptionsToStripeRectorTest
+ */
+final class CashierStripeOptionsToStripeRector extends AbstractScopeAwareRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Renames the Billable stripeOptions() to stripe().', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Cashier\Billable;
+
+class User extends Model
+{
+    use Billable;
+
+    public function stripeOptions(array $options = []) {
+        return [];
+    }
+}
+CODE_SAMPLE
+
+                ,
+                <<<'CODE_SAMPLE'
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Cashier\Billable;
+
+class User extends Model
+{
+    use Billable;
+
+    public function stripe(array $options = []) {
+        return [];
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param Class_ $node
+     * @param Scope $scope
+     *
+     * @return Node|null
+     */
+    public function refactorWithScope(Node $node, Scope $scope): ?Node
+    {
+        if (! $this->isObjectType($node, new ObjectType('Illuminate\Database\Eloquent\Model'))) {
+            return null;
+        }
+
+        if (! $this->usesBillableTrait($scope)) {
+            return null;
+        }
+
+        $stripeOptionsMethod = $node->getMethod('stripeOptions');
+
+        if (! $stripeOptionsMethod instanceof ClassMethod) {
+            return null;
+        }
+
+        $stripeOptionsMethod->name = new Identifier('stripe');
+
+        return $node;
+    }
+
+    private function usesBillableTrait(Scope $scope): bool
+    {
+        $classReflection = $scope->getClassReflection();
+
+        while ($classReflection instanceof ClassReflection) {
+            foreach ($classReflection->getTraits() as $traitReflection) {
+                if ($traitReflection->hasTraitUse('Laravel\Cashier\Billable')) {
+                    return true;
+                }
+            }
+
+            $classReflection = $classReflection->getParentClass();
+        }
+
+        return true;
+    }
+}
---
+++ stubs/Laravel/Cashier/Billable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Laravel\Cashier;
+
+if (class_exists('Laravel\Cashier\Billable')) {
+    return;
+}
+
+trait Billable
+{
+}
