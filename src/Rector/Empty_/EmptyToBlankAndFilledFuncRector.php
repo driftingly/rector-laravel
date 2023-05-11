64fb933faab5bc8c0c49928fa5b05dad2df2aea0
Peter Fox <peter.fox@peterfox.me>
2023-05-11 12:37:28 +0100

    Empty() to Blank() or Filled() rule

---
+++ src/Rector/Empty_/EmptyToBlankAndFilledFuncRector.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace RectorLaravel\Rector\Empty_;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\BooleanNot;
+use PhpParser\Node\Expr\Empty_;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+class EmptyToBlankAndFilledFuncRector extends AbstractRector
+{
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Replace use of the unsafe empty() function with Laravel\'s safer blank() & filled() functions.', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+empty([]);
+!empty([]);
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+blank([]);
+filled([]);
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [Empty_::class, BooleanNot::class];
+    }
+
+    public function refactor(Node $node): ?Node
+    {
+        if ($node instanceof BooleanNot) {
+            if (! $node->expr instanceof Empty_) {
+                return null;
+            }
+            $method = 'filled';
+            $args = [$node->expr->expr];
+        } else {
+            $method = 'blank';
+            $args = [$node->expr];
+        }
+
+        return $this->nodeFactory->createFuncCall($method, $args);
+    }
+}
