1216b910f74c5c0f7f3bfbc6e5a1e7bbd5b9fdef
geni_jaho <jahogeni@gmail.com>
2023-05-22 21:09:32 +0200

    Added rules for Cashier 14

---
+++ config/sets/packages/cashier/cashier-14.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
+
+# see https://github.com/laravel/cashier-stripe/blob/master/UPGRADE.md#upgrading-to-140-from-13x
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../config.php');
+
+    $rectorConfig->ruleWithConfiguration(RenameMethodRector::class, [
+        new MethodCallRename('Laravel\Cashier\Billable', 'removePaymentMethod', 'deletePaymentMethod'),
+        new MethodCallRename('Laravel\Cashier\Payment', 'isCancelled', 'isCanceled'),
+        new MethodCallRename('Laravel\Cashier\Subscription', 'cancelled', 'canceled'),
+        new MethodCallRename('Laravel\Cashier\Subscription', 'markAsCancelled', 'markAsCanceled'),
+    ]);
+};
---
+++ src/Set/Packages/Cashier/CashierLevelSetList.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RectorLaravel\Set\Packages\Cashier;
+
+use Rector\Set\Contract\SetListInterface;
+
+final class CashierLevelSetList implements SetListInterface
+{
+    /**
+     * @var string
+     */
+    final public const UP_TO_LARAVEL_CASHIER_14 = __DIR__ . '/../../config/sets/packages/cashier/level/up-to-cashier-14.php';
+}
