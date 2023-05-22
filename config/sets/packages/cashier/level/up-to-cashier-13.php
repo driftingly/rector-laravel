00afff93a3326fa25fbd205a525f02a12fc4e6ad
geni_jaho <jahogeni@gmail.com>
2023-05-22 21:42:56 +0200

    Small improvement and fixes

--- config/sets/packages/cashier/level/up-to-cashier-13.php
+++ config/sets/packages/cashier/level/up-to-cashier-13.php
@@ -3,9 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use RectorLaravel\Set\Packages\Cashier\CashierLevelSetList;
 use RectorLaravel\Set\Packages\Cashier\CashierSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([CashierSetList::LARAVEL_CASHIER_140, CashierLevelSetList::UP_TO_LARAVEL_CASHIER_13]);
+    $rectorConfig->sets([CashierSetList::LARAVEL_CASHIER_130]);
 };
--- src/Set/Packages/Cashier/CashierLevelSetList.php
+++ src/Set/Packages/Cashier/CashierLevelSetList.php
@@ -11,5 +11,10 @@ final class CashierLevelSetList implements SetListInterface
     /**
      * @var string
      */
+    final public const UP_TO_LARAVEL_CASHIER_13 = __DIR__ . '/../../config/sets/packages/cashier/level/up-to-cashier-13.php';
+
+    /**
+     * @var string
+     */
     final public const UP_TO_LARAVEL_CASHIER_14 = __DIR__ . '/../../config/sets/packages/cashier/level/up-to-cashier-14.php';
 }
