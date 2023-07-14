e79c716fd1c3531606da99354f36e3aeeaabfc62
Peter Fox <peter.fox@peterfox.me>
2023-07-14 10:25:03 +0100

    Fix up rule for Rector 0.17

--- src/Rector/FuncCall/SleepFuncToSleepStaticCallRector.php
+++ src/Rector/FuncCall/SleepFuncToSleepStaticCallRector.php
@@ -1,12 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace RectorLaravel\Rector\FuncCall;
 
 use PhpParser\Node;
-use PhpParser\Node\Expr\FuncCall;
-use PhpParser\Node\Expr\StaticCall;
 use Rector\Core\Rector\AbstractRector;
-use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -23,10 +22,12 @@ class SleepFuncToSleepStaticCallRector extends AbstractRector
                 new CodeSample(
                     <<<'CODE_SAMPLE'
 sleep(5);
-CODE_SAMPLE,
+CODE_SAMPLE
+,
                     <<<'CODE_SAMPLE'
 \Illuminate\Support\Sleep::sleep(5);
-CODE_SAMPLE,
+CODE_SAMPLE
+,
                 ),
             ]
         );
@@ -34,46 +35,26 @@ CODE_SAMPLE,
 
     public function getNodeTypes(): array
     {
-        return [FuncCall::class];
+        return [Node\Stmt\Expression::class];
     }
 
     /**
-     * @param FuncCall $node
+     * @param Node\Stmt\Expression $node
      */
-    public function refactor(Node $node): ?StaticCall
+    public function refactor(Node $node): ?Node
     {
-        if (! $this->isName($node->name, 'sleep') && ! $this->isName($node->name, 'usleep')) {
+        if (! $node->expr instanceof Node\Expr\FuncCall) {
             return null;
         }
 
-        if ($this->isReturnValueUsed($node)) {
+        if (! $this->isName($node->expr->name, 'sleep') && ! $this->isName($node->expr->name, 'usleep')) {
             return null;
         }
 
-        $method = $this->isName($node->name, 'sleep') ? 'sleep' : 'usleep';
+        $method = $this->isName($node->expr->name, 'sleep') ? 'sleep' : 'usleep';
 
-        return $this->nodeFactory->createStaticCall('Illuminate\Support\Sleep', $method, $node->args);
-    }
+        $node->expr = $this->nodeFactory->createStaticCall('Illuminate\Support\Sleep', $method, $node->expr->args);
 
-    protected function isReturnValueUsed(Node $node): bool
-    {
-        $parent = $node->getAttribute(AttributeKey::PARENT_NODE);
-        if ($parent === null) {
-            return false;
-        }
-
-        return $parent instanceof Node\Expr\Assign || $parent instanceof Node\Expr\AssignRef ||
-            $parent instanceof Node\Expr\BinaryOp || $parent instanceof Node\Expr\BooleanNot ||
-            $parent instanceof Node\Expr\Cast || $parent instanceof Node\Expr\Empty_ ||
-            $parent instanceof Node\Expr\ErrorSuppress || $parent instanceof Node\Expr\Exit_ ||
-            $parent instanceof Node\Expr\Include_ || $parent instanceof Node\Expr\Isset_ ||
-            $parent instanceof Node\Expr\List_ || $parent instanceof Node\Expr\Print_ ||
-            $parent instanceof Node\Expr\UnaryMinus || $parent instanceof Node\Expr\UnaryPlus ||
-            $parent instanceof Node\Expr\Yield_ || $parent instanceof Node\Expr\YieldFrom ||
-            $parent instanceof Node\Stmt\Case_ || $parent instanceof Node\Stmt\Else_ ||
-            $parent instanceof Node\Stmt\ElseIf_ || $parent instanceof Node\Stmt\For_ ||
-            $parent instanceof Node\Stmt\Foreach_ || $parent instanceof Node\Stmt\If_ ||
-            $parent instanceof Node\Stmt\Return_ || $parent instanceof Node\Stmt\Switch_ ||
-            $parent instanceof Node\Stmt\Throw_ || $parent instanceof Node\Stmt\While_;
+        return $node;
     }
 }
