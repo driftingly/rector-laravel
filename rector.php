6dcfe468b00d3888eac42b2e3de835187c36dd62
Anthony Clark <anthonyclark@gmail.com>
2024-02-07 10:37:01 -0800

    Update Rector to `1.0` (#176)
    * Update Rector to `1.0`
    * Update Rector config
    * Update readme

--- rector.php
+++ rector.php
@@ -4,15 +4,14 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->importNames();
-
-    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
-
-    $rectorConfig->skip([
+return RectorConfig::configure()
+    ->withImportNames()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSkip([
         // for tests
         '*/Source/*',
         '*/Fixture/*',
@@ -20,7 +19,6 @@ return static function (RectorConfig $rectorConfig): void {
         // skip for handle scoped, like in the rector-src as well
         // @see https://github.com/rectorphp/rector-src/blob/7f73cf017214257c170d34db3af7283eaeeab657/rector.php#L71
         StringClassNameToClassConstantRector::class,
-    ]);
-
-    $rectorConfig->sets([LevelSetList::UP_TO_PHP_82, SetList::DEAD_CODE, SetList::CODE_QUALITY, SetList::NAMING]);
-};
+    ])
+    ->withPhpSets()
+    ->withPreparedSets(deadCode: true, codeQuality: true, naming: true);
