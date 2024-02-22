9b01c6c153d7f7cec5da616391f6f193484e9f78
Peter Fox <peter.fox@peterfox.me>
2024-02-22 21:15:39 +0000

    wip

--- src/Rector/New_/NewInstanceToAppMakeRector.php
+++ src/Rector/New_/NewInstanceToAppMakeRector.php
@@ -6,14 +6,16 @@ use PhpParser\Node;
 use PhpParser\Node\Expr\New_;
 use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Name;
+use Rector\Contract\Rector\ConfigurableRectorInterface;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use Webmozart\Assert\Assert;
 
 /**
  * @see \RectorLaravel\Tests\Rector\New_\NewInstanceToAppMakeRector\NewInstanceToAppMakeRectorTest
  */
-class NewInstanceToAppMakeRector extends AbstractRector
+class NewInstanceToAppMakeRector extends AbstractRector implements ConfigurableRectorInterface
 {
     public function getRuleDefinition(): RuleDefinition
     {
@@ -52,5 +54,12 @@ CODE_SAMPLE,
                 ]
             )
         );
+    }
+
+    public function configure(array $configuration): void
+    {
+        Assert::allString($configuration);
+
+
     }
 }
