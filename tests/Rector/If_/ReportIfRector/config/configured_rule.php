c4a78151fbb37102f92c28fecf25203f820e8342
Peter Fox <peter.fox@peterfox.me>
2024-03-02 23:35:44 +0000

    Adds the report if rule

---
+++ tests/Rector/If_/ReportIfRector/config/configured_rule.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+use Rector\Config\RectorConfig;
+use RectorLaravel\Rector\If_\ReportIfRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+
+    $rectorConfig->rule(ReportIfRector::class);
+};
