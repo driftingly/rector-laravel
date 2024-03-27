4a13c859a7463d405dae5d7eb29fd9a5a677fb40
Peter Fox <peter.fox@peterfox.me>
2024-03-27 16:17:06 +0000

    [Laravel110] Blueprint geometry columns (#201)
    * Adds RefactorBlueprintGeometryColumnsRector rule

---
+++ src/Rector/MethodCall/RefactorBlueprintGeometryColumnsRector.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace RectorLaravel\Rector\MethodCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Scalar\String_;
+use PHPStan\Type\ObjectType;
+use Rector\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \RectorLaravel\Tests\Rector\MethodCall\RefactorBlueprintGeometryColumnsRector\RefactorBlueprintGeometryColumnsRectorTest
+ */
+class RefactorBlueprintGeometryColumnsRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'refactors calls with the pre Laravel 11 methods for blueprint geometry columns',
+            [new CodeSample(<<<'CODE_SAMPLE'
+$blueprint->point('coordinates')->spatialIndex();
+CODE_SAMPLE,
+                <<<'CODE_SAMPLE'
+$blueprint->geometry('coordinates', 'point')->spatialIndex();
+CODE_SAMPLE,
+            )]
+        );
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class];
+    }
+
+    /**
+     * @param  Node\Expr\MethodCall  $node
+     */
+    public function refactor(Node $node): ?MethodCall
+    {
+        if (! $this->isNames($node->name, [
+            'point',
+            'linestring',
+            'polygon',
+            'geometrycollection',
+            'multipoint',
+            'multilinestring',
+            'multipolygon',
+
+        ])) {
+            return null;
+        }
+
+        if (! $node->name instanceof Identifier) {
+            return null;
+        }
+
+        if (! $this->isObjectType($node->var, new ObjectType('Illuminate\Database\Schema\Blueprint'))) {
+            return null;
+        }
+
+        $previousName = $node->name;
+
+        $node->name = new Identifier('geometry');
+        $node->args[] = new Arg(new String_($previousName->name));
+
+        return $node;
+    }
+}
