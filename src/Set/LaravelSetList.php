7e300de28f6e2c7bca320e4713812fae185a37f6
Peter Fox <peter.fox@peterfox.me>
2024-05-07 20:58:24 +0100

    Document the set

--- src/Set/LaravelSetList.php
+++ src/Set/LaravelSetList.php
@@ -137,4 +137,9 @@ final class LaravelSetList implements SetListInterface
      * @var string
      */
     final public const LARAVEL_STATIC_TO_INJECTION = __DIR__ . '/../../config/sets/laravel-static-to-injection.php';
+
+    /**
+     * @var string
+     */
+    final public const LARAVEL_TYPE_DECLARATIONS = __DIR__ . '/../../config/sets/type-declaration.php';
 }
