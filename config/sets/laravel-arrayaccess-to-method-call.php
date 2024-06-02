50dd0cf9161d6b27c13030aac4505ff744a2417f
Peter Fox <peter.fox@peterfox.me>
2024-06-02 12:48:43 +0100

    Adds the ArrayAccess to Method Call set (#218)
    * Adds the ArrayAccess set
    
    * fixes
    
    * More fixes

---
+++ config/sets/laravel-arrayaccess-to-method-call.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+use PHPStan\Type\ObjectType;
+use Rector\Config\RectorConfig;
+use Rector\Transform\Rector\ArrayDimFetch\ArrayDimFetchToMethodCallRector;
+use Rector\Transform\ValueObject\ArrayDimFetchToMethodCall;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../config.php');
+
+    $rectorConfig
+        ->ruleWithConfiguration(
+            ArrayDimFetchToMethodCallRector::class,
+            [
+                new ArrayDimFetchToMethodCall(
+                    new ObjectType('Illuminate\Foundation\Application'),
+                    'make',
+                ),
+                new ArrayDimFetchToMethodCall(
+                    new ObjectType('Illuminate\Config\Repository'),
+                    'get',
+                ),
+            ],
+        );
+};
