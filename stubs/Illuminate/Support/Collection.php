6ee31a5bfc74eed87a901960df9cd371ca5ddd93
Benedikt Franke <benedikt.franke@mll.com>
2024-07-22 15:52:07 +0200

    try adding stub

---
+++ stubs/Illuminate/Support/Collection.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Illuminate\Support;
+
+if (class_exists('Illuminate\Support\Collection')) {
+    return;
+}
+
+/**
+ * @template TKey of array-key
+ *
+ * @template-covariant TValue
+ *
+ * @implements \Illuminate\Support\Enumerable<TKey, TValue>
+ */
+class Collection implements Enumerable
+{
+}
