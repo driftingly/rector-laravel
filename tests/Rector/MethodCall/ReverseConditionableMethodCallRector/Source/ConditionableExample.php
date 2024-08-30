037f9974028614b3eb635c622e103c0ec17a61d6
Peter Fox <peter.fox@peterfox.me>
2024-08-30 22:03:04 +0100

    Adds the ReverseCondiationableMethodCallRector (#245)
    * Adds the ReverseCondiationableMethodCallRector
    
    * fix docs

---
+++ tests/Rector/MethodCall/ReverseConditionableMethodCallRector/Source/ConditionableExample.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RectorLaravel\Tests\Rector\MethodCall\ReverseConditionableMethodCallRector\Source;
+
+use Illuminate\Support\Traits\Conditionable;
+
+class ConditionableExample
+{
+    use Conditionable;
+}
