e2e7899567b675ab10bd1444b13942b1304b051b
Geni Jaho <jahogeni@gmail.com>
2024-09-20 21:02:27 +0200

    Add new rule AssertSeeToAssertSeeHtmlRector (#251)
    * Add new rule AssertSeeToAssertSeeHtmlRector
    
    * Add AssertSeeToAssertSeeHtmlRector to Laravel 11 set
    
    * Refactored methods to replace into a protected property

---
+++ src/Rector/MethodCall/AssertSeeToAssertSeeHtmlRector.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RectorLaravel\Rector\MethodCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Type\ObjectType;
+use Rector\PhpParser\Node\Value\ValueResolver;
+use Rector\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \RectorLaravel\Tests\Rector\MethodCall\AssertSeeToAssertSeeHtmlRector\AssertSeeToAssertSeeHtmlRectorTest
+ */
+final class AssertSeeToAssertSeeHtmlRector extends AbstractRector
+{
+    /**
+     * @var string[]
+     */
+    protected array $methodsToReplace = [
+        'assertSee' => 'assertSeeHtml',
+        'assertDontSee' => 'assertDontSeeHtml',
+        'assertSeeInOrder' => 'assertSeeHtmlInOrder',
+    ];
+
+    public function __construct(
+        private readonly ValueResolver $valueResolver
+    ) {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Replace assertSee with assertSeeHtml when testing HTML with escape set to false',
+            [
+                new CodeSample(
+                    '$response->assertSee("<li>foo</li>", false);',
+                    '$response->assertSeeHtml("<li>foo</li>");'
+                ),
+                new CodeSample(
+                    '$response->assertDontSee("<li>foo</li>", false);',
+                    '$response->assertDontSeeHtml("<li>foo</li>");'
+                ),
+                new CodeSample(
+                    '$response->assertSeeInOrder(["<li>foo</li>", "<li>bar</li>"], false);',
+                    '$response->assertSeeHtmlInOrder(["<li>foo</li>", "<li>bar</li>"]);'
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class];
+    }
+
+    /**
+     * @param  MethodCall  $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isObjectType($node->var, new ObjectType('Illuminate\Testing\TestResponse'))) {
+            return null;
+        }
+
+        $methodCallName = (string) $this->getName($node->name);
+
+        if (! array_key_exists($methodCallName, $this->methodsToReplace)) {
+            return null;
+        }
+
+        if (count($node->getArgs()) !== 2) {
+            return null;
+        }
+
+        if (! $this->valueResolver->isFalse($node->getArgs()[1]->value)) {
+            return null;
+        }
+
+        return $this->nodeFactory->createMethodCall(
+            $node->var,
+            $this->methodsToReplace[$methodCallName],
+            [$node->getArgs()[0]]
+        );
+    }
+}
---
+++ tests/Rector/MethodCall/AssertSeeToAssertSeeHtmlRector/config/configured_rule.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Rector\MethodCall\AssertSeeToAssertSeeHtmlRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+
+    $rectorConfig->rule(AssertSeeToAssertSeeHtmlRector::class);
+};
