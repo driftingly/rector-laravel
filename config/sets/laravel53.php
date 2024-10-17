5d07152cdfa0b25e7f4db6c2738933087a1c30a8
geni_jaho <jahogeni@gmail.com>
2024-10-17 18:33:17 +0200

    Migrate the deprecated lists method in Laravel 5.3

--- config/sets/laravel53.php
+++ config/sets/laravel53.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Removing\Rector\Class_\RemoveTraitUseRector;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
@@ -11,5 +13,12 @@ return static function (RectorConfig $rectorConfig): void {
         ->ruleWithConfiguration(RemoveTraitUseRector::class, [
             // see https://laravel.com/docs/5.3/upgrade
             'Illuminate\Foundation\Auth\Access\AuthorizesResources',
+        ]);
+
+    $rectorConfig
+        ->ruleWithConfiguration(RenameMethodRector::class, [
+            // https://laravel.com/docs/5.3/upgrade#5.2-deprecations
+            new MethodCallRename('Illuminate\Support\Collection', 'lists', 'pluck'),
+            new MethodCallRename('Illuminate\Database\Query\Builder', 'lists', 'pluck'),
         ]);
 };
