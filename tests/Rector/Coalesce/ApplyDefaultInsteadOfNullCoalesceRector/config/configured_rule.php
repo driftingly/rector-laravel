fc9df7179bcacfa028e007911a5282a80c427a43
Peter Fox <peter.fox@peterfox.me>
2024-10-27 09:02:02 +0000

    All positive test cases

--- tests/Rector/Coalesce/ApplyDefaultInsteadOfNullCoalesceRector/config/configured_rule.php
+++ tests/Rector/Coalesce/ApplyDefaultInsteadOfNullCoalesceRector/config/configured_rule.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use RectorLaravel\Rector\Coalesce\ApplyDefaultInsteadOfNullCoalesceRector;
 use RectorLaravel\ValueObject\ApplyDefaultWithFuncCall;
+use PHPStan\Type\ObjectType;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
 
     $rectorConfig->ruleWithConfiguration(ApplyDefaultInsteadOfNullCoalesceRector::class, [
         new ApplyDefaultWithFuncCall('config'),
+        new \RectorLaravel\ValueObject\ApplyDefaultWithMethodCall(new ObjectType('Illuminate\Http\Request'), 'input'),
+        new \RectorLaravel\ValueObject\ApplyDefaultWithStaticCall(new ObjectType('Illuminate\Support\Env'), 'get'),
     ]);
 };
