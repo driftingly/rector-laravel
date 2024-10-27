044b5dfcbf7e0cd9d841a7101b72188f89d4875b
Peter Fox <peter.fox@peterfox.me>
2024-10-27 08:57:37 +0000

    Working in all scenarios

--- src/Rector/Coalesce/ApplyDefaultInsteadOfNullCoalesceRector.php
+++ src/Rector/Coalesce/ApplyDefaultInsteadOfNullCoalesceRector.php
@@ -3,6 +3,11 @@
 namespace RectorLaravel\Rector\Coalesce;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\BinaryOp\Coalesce;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\StaticCall;
 use Rector\Contract\Rector\ConfigurableRectorInterface;
 use Rector\Rector\AbstractRector;
 use RectorLaravel\ValueObject\ApplyDefaultWithFuncCall;
@@ -17,7 +22,6 @@ use Webmozart\Assert\Assert;
  */
 final class ApplyDefaultInsteadOfNullCoalesceRector extends AbstractRector implements ConfigurableRectorInterface
 {
-
     /**
      * @var array<int, ApplyDefaultWithFuncCall|ApplyDefaultWithMethodCall|ApplyDefaultWithStaticCall>
      */
@@ -35,42 +39,58 @@ CODE_SAMPLE,
                     <<<'CODE_SAMPLE'
 config('app.name', 'Laravel');
 CODE_SAMPLE
-                )
+                ),
             ]
         );
     }
 
     public function getNodeTypes(): array
     {
-        return [Node\Expr\BinaryOp\Coalesce::class];
+        return [Coalesce::class];
     }
 
     /**
-     * @param Node\Expr\BinaryOp\Coalesce $node
-     * @return Node\Expr\MethodCall|Node\Expr\StaticCall|Node\Expr\FuncCall|null
+     * @param  Coalesce  $node
      */
-    public function refactor(Node $node): Node\Expr\MethodCall|Node\Expr\StaticCall|Node\Expr\FuncCall|null
+    public function refactor(Node $node): MethodCall|StaticCall|FuncCall|null
     {
+        if (! $node->left instanceof FuncCall &&
+            ! $node->left instanceof MethodCall &&
+            ! $node->left instanceof StaticCall
+        ) {
+            return null;
+        }
+
+        if ($node->left->isFirstClassCallable()) {
+            return null;
+        }
+
+        $call = $node->left;
+
         foreach ($this->applyDefaultWith as $applyDefaultWith) {
-            if (! $applyDefaultWith instanceof ApplyDefaultWithFuncCall || ! $node->left instanceof Node\Expr\FuncCall) {
-                return null;
-            }
 
-            if (! $this->isName($node->left, $applyDefaultWith->getFunctionName())) {
-                return null;
-            }
+            if ($applyDefaultWith instanceof ApplyDefaultWithFuncCall) {
+                if ($call instanceof FuncCall && ! $this->isName($call, $applyDefaultWith->getFunctionName())) {
+                    continue;
+                }
+            } elseif ($applyDefaultWith instanceof ApplyDefaultWithMethodCall) {
+                if ($call instanceof MethodCall && (! $this->isObjectType($call->var, $applyDefaultWith->getObjectType()) ||
+                    $this->isName($call, $applyDefaultWith->getMethodName()))) {
+                    continue;
+                }
 
-            $call = $node->left;
-
-            if ($call->isFirstClassCallable()) {
-                return null;
+            } elseif ($applyDefaultWith instanceof ApplyDefaultWithStaticCall) {
+                if ($call instanceof StaticCall && (! $this->isObjectType($call->class, $applyDefaultWith->getObjectType()) ||
+                    $this->isName($call, $applyDefaultWith->getMethodName()))) {
+                    continue;
+                }
             }
 
             if (count($call->args) === $applyDefaultWith->getArgumentPosition()) {
                 if ($this->getType($node->right)->isNull()->yes()) {
                     return $call;
                 }
-                $call->args[count($call->args)] = new Node\Arg($node->right);
+                $call->args[count($call->args)] = new Arg($node->right);
 
                 return $call;
             }
@@ -79,9 +99,6 @@ CODE_SAMPLE
         return null;
     }
 
-    /**
-     * @param array<int, ApplyDefaultWithFuncCall|ApplyDefaultWithStaticCall|ApplyDefaultWithMethodCall> $configuration
-     */
     public function configure(array $configuration): void
     {
         Assert::allIsInstanceOfAny($configuration, [
--- tests/Rector/Coalesce/ApplyDefaultInsteadOfNullCoalesceRector/config/configured_rule.php
+++ tests/Rector/Coalesce/ApplyDefaultInsteadOfNullCoalesceRector/config/configured_rule.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use RectorLaravel\Rector\Coalesce\ApplyDefaultInsteadOfNullCoalesceRector;
+use RectorLaravel\ValueObject\ApplyDefaultWithFuncCall;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
 
     $rectorConfig->ruleWithConfiguration(ApplyDefaultInsteadOfNullCoalesceRector::class, [
-        new \RectorLaravel\ValueObject\ApplyDefaultWithFuncCall('config'),
+        new ApplyDefaultWithFuncCall('config'),
     ]);
 };
