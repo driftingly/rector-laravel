a1a41cec5967a3a57297206fa06576ce1b9795d7
Peter Fox <peter.fox@peterfox.me>
2024-11-02 11:03:24 +0000

    use data_get and position argument

--- src/Rector/Coalesce/ApplyDefaultInsteadOfNullCoalesceRector.php
+++ src/Rector/Coalesce/ApplyDefaultInsteadOfNullCoalesceRector.php
@@ -40,6 +40,7 @@ final class ApplyDefaultInsteadOfNullCoalesceRector extends AbstractRector imple
         return [
             new ApplyDefaultInsteadOfNullCoalesce('config'),
             new ApplyDefaultInsteadOfNullCoalesce('env'),
+            new ApplyDefaultInsteadOfNullCoalesce('data_get', argumentPosition: 2),
             new ApplyDefaultInsteadOfNullCoalesce('input', new ObjectType('Illuminate\Http\Request')),
             new ApplyDefaultInsteadOfNullCoalesce('get', new ObjectType('Illuminate\Support\Env')),
         ];
