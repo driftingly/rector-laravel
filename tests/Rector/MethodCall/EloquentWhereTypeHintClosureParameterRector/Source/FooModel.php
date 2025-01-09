fc1c2cf2a1efaca7e760244ae8dc5785347b8eae
Peter Fox <peter.fox@peterfox.me>
2025-01-09 23:35:29 +0000

    Update/Improve tests

---
+++ tests/Rector/MethodCall/EloquentWhereTypeHintClosureParameterRector/Source/FooModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RectorLaravel\Tests\Rector\MethodCall\EloquentWhereTypeHintClosureParameterRector\Source;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FooModel extends Model
+{
+
+}
