e7de22ea2e8a758f882dae8bd14fe984d30294f8
Peter Fox <peter.fox@peterfox.me>
2025-03-20 21:50:53 +0000

    Adds a rule to apply scope attributes on methods over using scope methods

--- config/sets/laravel120.php
+++ config/sets/laravel120.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use RectorLaravel\Rector\ClassMethod\ScopeNamedClassMethodToScopeAttributedClassMethodRector;
 use RectorLaravel\Rector\MethodCall\ContainerBindConcreteWithClosureOnlyRector;
 
 // see https://laravel.com/docs/12.x/upgrade
@@ -11,4 +12,6 @@ return static function (RectorConfig $rectorConfig): void {
 
     // https://github.com/laravel/framework/pull/54628
     $rectorConfig->rule(ContainerBindConcreteWithClosureOnlyRector::class);
+
+    $rectorConfig->rule(ScopeNamedClassMethodToScopeAttributedClassMethodRector::class);
 };
