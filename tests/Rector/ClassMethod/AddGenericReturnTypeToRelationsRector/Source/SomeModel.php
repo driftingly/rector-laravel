578ec52dc2d7e927f01bb4c8a221f42f282cbe53
Jordan Crocker <jordan.crocker@crowdcube.com>
2025-03-26 09:20:09 +0000

    Move model to `Source` directory

---
+++ tests/Rector/ClassMethod/AddGenericReturnTypeToRelationsRector/Source/SomeModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RectorLaravel\Tests\Rector\ClassMethod\AddGenericReturnTypeToRelationsRector\Source;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SomeModel extends Model
+{
+
+}
