169cbec9447c896a8f9b692a350853b1dea3be87
Caleb White <cdwhite3@pm.me>
2025-08-19 12:16:38 -0500

    feat: improve AppEnvironmentComparisonToParameterRector (#368)
    This adds several improvements:
    - support for the `in_array` function
    - support for != and !== operators
    - support for the cleaner `isLocal` and `isProduction` methods

--- src/Rector/Expr/AppEnvironmentComparisonToParameterRector.php
+++ src/Rector/Expr/AppEnvironmentComparisonToParameterRector.php
@@ -7,10 +7,16 @@ namespace RectorLaravel\Rector\Expr;
 use PhpParser\Node;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\BinaryOp;
 use PhpParser\Node\Expr\BinaryOp\Equal;
 use PhpParser\Node\Expr\BinaryOp\Identical;
+use PhpParser\Node\Expr\BinaryOp\NotEqual;
+use PhpParser\Node\Expr\BinaryOp\NotIdentical;
+use PhpParser\Node\Expr\BooleanNot;
+use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Identifier;
 use PhpParser\Node\Scalar\String_;
 use PHPStan\Type\ObjectType;
 use RectorLaravel\AbstractRector;
@@ -25,82 +31,145 @@ class AppEnvironmentComparisonToParameterRector extends AbstractRector
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition(
-            'Replace `$app->environment() === \'local\'` with `$app->environment(\'local\')`',
+            'Replace app environment comparison with parameter or method call',
             [
                 new CodeSample(
                     <<<'CODE_SAMPLE'
-$app->environment() === 'production';
+$app->environment() === 'local';
+$app->environment() !== 'production';
+$app->environment() === 'testing';
+in_array($app->environment(), ['local', 'testing']);
 CODE_SAMPLE
                     ,
                     <<<'CODE_SAMPLE'
-$app->environment('production');
+$app->isLocal();
+! $app->isProduction();
+$app->environment('testing');
+$app->environment(['local', 'testing']);
 CODE_SAMPLE
                 ),
             ]
         );
     }
 
+    /** @return list<class-string<Node>> */
     public function getNodeTypes(): array
     {
-        return [Expr::class];
+        return [FuncCall::class, Identical::class, Equal::class, NotIdentical::class, NotEqual::class];
     }
 
-    public function refactor(Node $node): MethodCall|StaticCall|null
+    /** @param FuncCall|Identical|Equal|NotIdentical|NotEqual $node */
+    public function refactor(Node $node): ?Expr
     {
-        if (! $node instanceof Identical && ! $node instanceof Equal) {
+        if ($node instanceof FuncCall) {
+            [$methodCall, $otherNode] = $this->handleFuncCall($node);
+        } else {
+            [$methodCall, $otherNode] = $this->handleBinaryOp($node);
+        }
+
+        if ($methodCall === null || $otherNode === null) {
             return null;
         }
 
-        /** @var MethodCall|StaticCall|null $methodCall */
-        $methodCall = array_values(
-            array_filter(
-                [$node->left, $node->right],
-                fn ($node) => ($node instanceof MethodCall || $node instanceof StaticCall) && $this->isName(
-                    $node->name,
-                    'environment'
-                )
-            )
-        )[0] ?? null;
+        $methodName = null;
 
-        if ($methodCall === null || ! $this->validMethodCall($methodCall)) {
-            return null;
+        if ($otherNode instanceof String_) {
+            $methodName = $this->getMethodName($methodCall, $otherNode->value);
         }
 
-        /** @var Expr $otherNode */
-        $otherNode = array_values(
-            array_filter([$node->left, $node->right], static fn ($node) => $node !== $methodCall)
-        )[0] ?? null;
-
-        if (! $otherNode instanceof String_) {
-            return null;
+        if ($methodName === null) {
+            $methodCall->args[] = new Arg($otherNode);
+        } else {
+            $methodCall->name = new Identifier($methodName);
         }
 
-        // make sure the method call has no arguments
-        if ($methodCall->getArgs() !== []) {
-            return null;
+        if ($node instanceof NotIdentical || $node instanceof NotEqual) {
+            return new BooleanNot($methodCall);
         }
-
-        $methodCall->args[] = new Arg($otherNode);
 
         return $methodCall;
     }
 
-    private function validMethodCall(MethodCall|StaticCall $methodCall): bool
+    /** @return array{0: MethodCall|StaticCall|null, 1: Expr|null} */
+    private function handleFuncCall(FuncCall $funcCall): array
     {
+        if (! $this->isName($funcCall->name, 'in_array')) {
+            return [null, null];
+        }
+
+        $methodCall = $funcCall->getArg('needle', 0);
+        $haystack = $funcCall->getArg('haystack', 1);
+
+        if (! $haystack instanceof Arg || ! $methodCall instanceof Arg || ! $this->validMethodCall($methodCall->value)) {
+            return [null, null];
+        }
+
+        return [$methodCall->value, $haystack->value];
+    }
+
+    /** @return array{0: MethodCall|StaticCall|null, 1: Expr|null} */
+    private function handleBinaryOp(BinaryOp $binaryOp): array
+    {
+        $methodCall = array_values(
+            array_filter(
+                [$binaryOp->left, $binaryOp->right],
+                fn ($node) => $this->validMethodCall($node),
+            )
+        )[0] ?? null;
+
+        $otherNode = array_values(
+            array_filter([$binaryOp->left, $binaryOp->right], static fn ($node) => $node !== $methodCall)
+        )[0] ?? null;
+
+        return [$methodCall, $otherNode];
+    }
+
+    /** @phpstan-assert-if-true MethodCall|StaticCall $node */
+    private function validMethodCall(Node $node): bool
+    {
+        if (! $node instanceof MethodCall && ! $node instanceof StaticCall) {
+            return false;
+        }
+
+        if (! $this->isName($node->name, 'environment')) {
+            return false;
+        }
+
+        // make sure the method call has no arguments
+        if ($node->getArgs() !== []) {
+            return false;
+        }
+
         return match (true) {
-            $methodCall instanceof MethodCall && $this->isObjectType(
-                $methodCall->var,
+            $node instanceof MethodCall && $this->isObjectType(
+                $node->var,
                 new ObjectType('Illuminate\Contracts\Foundation\Application')
             ) => true,
-            $methodCall instanceof StaticCall && $this->isObjectType(
-                $methodCall->class,
+            $node instanceof StaticCall && $this->isObjectType(
+                $node->class,
                 new ObjectType('Illuminate\Support\Facades\App')
             ) => true,
-            $methodCall instanceof StaticCall && $this->isObjectType(
-                $methodCall->class,
+            $node instanceof StaticCall && $this->isObjectType(
+                $node->class,
                 new ObjectType('App')
             ) => true,
             default => false,
+        };
+    }
+
+    private function getMethodName(MethodCall|StaticCall $methodCall, string $environment): ?string
+    {
+        if (
+            $methodCall instanceof MethodCall
+            && ! $this->isObjectType($methodCall->var, new ObjectType('Illuminate\Foundation\Application'))
+        ) {
+            return null;
+        }
+
+        return match ($environment) {
+            'local' => 'isLocal',
+            'production' => 'isProduction',
+            default => null,
         };
     }
 }
