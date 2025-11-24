16c9b5e1ff380db6dc47cc012e215cc6bdeeb082
Peter Fox <peter.fox@peterfox.me>
2025-11-24 04:32:08 +0000

    Improve first rule

--- tests/Rector/MethodCall/EloquentWhereRelationTypeHintingParameterRector/Source/FooModel.php
+++ tests/Rector/MethodCall/EloquentWhereRelationTypeHintingParameterRector/Source/FooModel.php
@@ -3,5 +3,12 @@
 namespace RectorLaravel\Tests\Rector\MethodCall\EloquentWhereRelationTypeHintingParameterRector\Source;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class FooModel extends Model {}
+class FooModel extends Model
+{
+    public function relation(): HasMany
+    {
+        return new HasMany;
+    }
+}
