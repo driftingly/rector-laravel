b9e989c2d4be4d04582f8cdfd59f9b5c0c4c7103
Caleb White <cdwhite3@pm.me>
2025-11-29 12:28:35 -0600

    feat: add app to resolve rector (#430)
    * feat: add app to resolve rector
    
    * chore: update rectors to also check for resolve()

---
+++ src/Rector/FuncCall/AppToResolveRector.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace RectorLaravel\Rector\FuncCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Name;
+use RectorLaravel\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \RectorLaravel\Tests\Rector\FuncCall\AppToResolveRector\AppToResolveRectorTest
+ */
+class AppToResolveRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Convert app() to resolve() where applicable.',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+app('foo');
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+resolve('foo');
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [FuncCall::class];
+    }
+
+    /**
+     * @param  FuncCall  $node
+     */
+    public function refactor(Node $node): ?FuncCall
+    {
+        if (! $this->isName($node->name, 'app')) {
+            return null;
+        }
+
+        $abstract = $node->getArg('abstract', 0);
+
+        if ($abstract === null) {
+            return null;
+        }
+
+        if ($this->getType($abstract->value)->isNull()->yes()) {
+            return null;
+        }
+
+        $node->name = new Name('resolve');
+
+        return $node;
+    }
+}
