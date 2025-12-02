afd605f19b57e70e33e03e19119b0d6743550098
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-12-02 22:05:32 +0700

    fix phpstan

--- src/Rector/ArrayDimFetch/ArrayToArrGetRector.php
+++ src/Rector/ArrayDimFetch/ArrayToArrGetRector.php
@@ -94,7 +94,7 @@ CODE_SAMPLE
             return null;
         }
 
-        if ($node->getAttribute(AttributeKey::IS_BEING_ASSIGNED)) {
+        if ($node->getAttribute(AttributeKey::IS_BEING_ASSIGNED) === true) {
             return null;
         }
 
