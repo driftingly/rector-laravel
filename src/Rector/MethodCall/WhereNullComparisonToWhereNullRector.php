cede43432597bdc5bc68a308e8b9f8218f00a71e
Peter Fox <peter.fox@peterfox.me>
2025-12-16 12:44:12 +0000

    Fixup

--- src/Rector/MethodCall/WhereNullComparisonToWhereNullRector.php
+++ src/Rector/MethodCall/WhereNullComparisonToWhereNullRector.php
@@ -3,15 +3,16 @@
 namespace RectorLaravel\Rector\MethodCall;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
 use RectorLaravel\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 class WhereNullComparisonToWhereNullRector extends AbstractRector
 {
-
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Convert to where comparison to whereNull method call', [
@@ -19,22 +20,21 @@ class WhereNullComparisonToWhereNullRector extends AbstractRector
 $query->where('foo', null);
 $query->where('foo', '=', null);
 CODE_SAMPLE,
-            <<<'CODE_SAMPLE'
+                <<<'CODE_SAMPLE'
 $query->whereNull('foo');
 $query->whereNull('foo');
 CODE_SAMPLE
-            )
+            ),
         ]);
     }
 
     public function getNodeTypes(): array
     {
-        return [Node\Expr\MethodCall::class];
+        return [MethodCall::class];
     }
 
     /**
-     * @param Node\Expr\MethodCall $node
-     * @return Node\Expr\MethodCall|null
+     * @param  MethodCall  $node
      */
     public function refactor(Node $node): ?MethodCall
     {
@@ -53,15 +53,15 @@ CODE_SAMPLE
         $args = $node->args;
         $type = null;
 
-        if (count($args) === 2 && $args[1] instanceof Node\Arg) {
+        if (count($args) === 2 && $args[1] instanceof Arg) {
             $type = $this->getType($args[1]->value);
         }
 
-        if (count($args) === 3 && $args[2] instanceof Node\Arg) {
+        if (count($args) === 3 && $args[2] instanceof Arg) {
             $type = $this->getType($args[2]->value);
         }
 
-        if ($type === null) {
+        if (! $type instanceof Type) {
             return null;
         }
 
