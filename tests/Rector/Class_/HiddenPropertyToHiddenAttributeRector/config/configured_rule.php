42fc605808a46239a4aa232a0f188eea5f5e81d9
Peter Fox <peter.fox@peterfox.me>
2026-03-03 09:58:57 +0000

    Adds the hidden attribute rule

--- tests/Rector/Class_/HiddenPropertyToHiddenAttributeRector/config/configured_rule.php
+++ tests/Rector/Class_/HiddenPropertyToHiddenAttributeRector/config/configured_rule.php
@@ -3,13 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use RectorLaravel\Rector\Class_\FillablePropertyToFillableAttributeRector;
 use RectorLaravel\Rector\Class_\HiddenPropertyToHiddenAttributeRector;
 
-// see https://laravel.com/docs/13.x/upgrade
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/../config.php');
-
-    $rectorConfig->rule(FillablePropertyToFillableAttributeRector::class);
     $rectorConfig->rule(HiddenPropertyToHiddenAttributeRector::class);
 };
