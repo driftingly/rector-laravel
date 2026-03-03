4b2fc23eeac2a8d914024629f4ca57426f0eb2ef
Peter Fox <peter.fox@peterfox.me>
2026-03-03 10:22:23 +0000

    Adds guarded rule

--- config/sets/laravel130.php
+++ config/sets/laravel130.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use RectorLaravel\Rector\Class_\AppendsPropertyToAppendsAttributeRector;
 use RectorLaravel\Rector\Class_\FillablePropertyToFillableAttributeRector;
+use RectorLaravel\Rector\Class_\GuardedPropertyToGuardedAttributeRector;
 use RectorLaravel\Rector\Class_\HiddenPropertyToHiddenAttributeRector;
 use RectorLaravel\Rector\Class_\TablePropertyToTableAttributeRector;
 
@@ -14,6 +15,7 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->rule(AppendsPropertyToAppendsAttributeRector::class);
     $rectorConfig->rule(FillablePropertyToFillableAttributeRector::class);
+    $rectorConfig->rule(GuardedPropertyToGuardedAttributeRector::class);
     $rectorConfig->rule(HiddenPropertyToHiddenAttributeRector::class);
     $rectorConfig->rule(TablePropertyToTableAttributeRector::class);
 };
