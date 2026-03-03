56d9a9a2ed20a34906c7db434feb48ee8e2be3c3
Peter Fox <peter.fox@peterfox.me>
2026-03-03 10:21:44 +0000

    Adds appends rule

--- config/sets/laravel130.php
+++ config/sets/laravel130.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use RectorLaravel\Rector\Class_\AppendsPropertyToAppendsAttributeRector;
 use RectorLaravel\Rector\Class_\FillablePropertyToFillableAttributeRector;
 use RectorLaravel\Rector\Class_\HiddenPropertyToHiddenAttributeRector;
 use RectorLaravel\Rector\Class_\TablePropertyToTableAttributeRector;
@@ -11,6 +12,7 @@ use RectorLaravel\Rector\Class_\TablePropertyToTableAttributeRector;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
 
+    $rectorConfig->rule(AppendsPropertyToAppendsAttributeRector::class);
     $rectorConfig->rule(FillablePropertyToFillableAttributeRector::class);
     $rectorConfig->rule(HiddenPropertyToHiddenAttributeRector::class);
     $rectorConfig->rule(TablePropertyToTableAttributeRector::class);
