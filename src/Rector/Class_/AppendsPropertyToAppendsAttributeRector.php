c4dd2e3381b0310e55a21eed934ac54ee5aed61a
Peter Fox <peter.fox@peterfox.me>
2026-03-15 22:08:20 +0000

    Laravel 13 Model Attributes (#477)
    * Laravel 13 attributes
    
    * Adds the hidden attribute rule
    
    * Adds the table attribute
    
    * Adds appends rule
    
    * Adds guarded rule
    
    * Adds the touches rules
    
    * Adds the connection attribute
    
    * Docs update
    
    * swap use of Name to FullyQualifiedName
    
    * Fix issues with existing attributes
    
    * Handle empty arrays
    
    * additional test

---
+++ src/Rector/Class_/AppendsPropertyToAppendsAttributeRector.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RectorLaravel\Rector\Class_;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Attribute;
+use PhpParser\Node\AttributeGroup;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Class_;
+use PHPStan\Type\ObjectType;
+use Rector\Php80\NodeAnalyzer\PhpAttributeAnalyzer;
+use RectorLaravel\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \RectorLaravel\Tests\Rector\Class_\AppendsPropertyToAppendsAttributeRector\AppendsPropertyToAppendsAttributeRectorTest
+ */
+final class AppendsPropertyToAppendsAttributeRector extends AbstractRector
+{
+    public function __construct(private readonly PhpAttributeAnalyzer $phpAttributeAnalyzer) {}
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Changes model appends property to use the appends attribute',
+            [new CodeSample(
+                <<<'CODE_SAMPLE'
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $appends = [
+        'full_name',
+    ];
+}
+CODE_SAMPLE,
+                <<<'CODE_SAMPLE'
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Appends;
+
+#[Appends(['full_name'])]
+class User extends Model
+{
+}
+CODE_SAMPLE
+            )]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param  Class_  $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isObjectType($node, new ObjectType('Illuminate\Database\Eloquent\Model'))) {
+            return null;
+        }
+
+        $appendsProperty = $node->getProperty('appends');
+        if ($appendsProperty === null) {
+            return null;
+        }
+
+        if (! $appendsProperty->isProtected()) {
+            return null;
+        }
+
+        $propertyProperty = $appendsProperty->props[0];
+        if ($propertyProperty->default === null || ! $propertyProperty->default instanceof Array_) {
+            return null;
+        }
+
+        $appendsArray = $propertyProperty->default;
+
+        if ($appendsArray->items === []) {
+            return null;
+        }
+
+        if (! $this->isArrayOfStrings($appendsArray)) {
+            return null;
+        }
+
+        if ($this->phpAttributeAnalyzer->hasPhpAttribute($node, 'Illuminate\Database\Eloquent\Attributes\Appends')) {
+            return null;
+        }
+
+        // Add attribute to class
+        $node->attrGroups[] = new AttributeGroup([
+            new Attribute(
+                new FullyQualified('Illuminate\Database\Eloquent\Attributes\Appends'),
+                [new Arg($appendsArray)]
+            ),
+        ]);
+
+        // Remove property
+        foreach ($node->stmts as $key => $stmt) {
+            if ($stmt === $appendsProperty) {
+                unset($node->stmts[$key]);
+                break;
+            }
+        }
+
+        return $node;
+    }
+
+    private function isArrayOfStrings(Array_ $array): bool
+    {
+        foreach ($array->items as $item) {
+            if ($item === null) {
+                return false;
+            }
+
+            if (! $item->value instanceof String_) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/Rector/Class_/TablePropertyToTableAttributeRector.php
@@ -0,0 +1,221 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RectorLaravel\Rector\Class_;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Property;
+use PHPStan\Type\ObjectType;
+use Rector\Php80\NodeAnalyzer\PhpAttributeAnalyzer;
+use RectorLaravel\AbstractRector;
+use RectorLaravel\NodeFactory\TableAttributeFactory;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \RectorLaravel\Tests\Rector\Class_\TablePropertyToTableAttributeRector\TablePropertyToTableAttributeRectorTest
+ */
+final class TablePropertyToTableAttributeRector extends AbstractRector
+{
+    public function __construct(
+        private readonly TableAttributeFactory $tableAttributeFactory,
+        private readonly PhpAttributeAnalyzer $phpAttributeAnalyzer,
+    ) {}
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Changes model table-related properties to use the Table attribute',
+            [new CodeSample(
+                <<<'CODE_SAMPLE'
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table = 'users';
+
+    protected $primaryKey = 'user_id';
+
+    protected $keyType = 'string';
+
+    protected $incrementing = false;
+}
+CODE_SAMPLE,
+                <<<'CODE_SAMPLE'
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Table;
+
+#[Table(table: 'users', key: 'user_id', keyType: 'string', incrementing: false)]
+class User extends Model
+{
+}
+CODE_SAMPLE
+            )]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param  Class_  $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isObjectType($node, new ObjectType('Illuminate\Database\Eloquent\Model'))) {
+            return null;
+        }
+
+        $tableAttributeClass = 'Illuminate\Database\Eloquent\Attributes\Table';
+        $hasExistingAttribute = $this->phpAttributeAnalyzer->hasPhpAttribute($node, $tableAttributeClass);
+
+        // Resolve the table value: from $table property, or from existing attribute
+        $tableProperty = $node->getProperty('table');
+        $tableValue = null;
+
+        if ($tableProperty !== null) {
+            if (! $tableProperty->isProtected()) {
+                return null;
+            }
+            $tableValue = $this->getPropertyDefaultValue($tableProperty);
+            if (! $tableValue instanceof String_) {
+                return null;
+            }
+        }
+
+        if (! $tableValue instanceof String_ && $hasExistingAttribute) {
+            $tableValue = $this->getExistingAttributeArg($node, $tableAttributeClass, 'table');
+            if (! $tableValue instanceof String_) {
+                return null;
+            }
+        }
+
+        if (! $tableValue instanceof String_) {
+            return null;
+        }
+
+        // Seed options from existing attribute args (properties take precedence below)
+        $options = [];
+        if ($hasExistingAttribute) {
+            foreach (['key', 'keyType', 'incrementing'] as $argName) {
+                $existingArg = $this->getExistingAttributeArg($node, $tableAttributeClass, $argName);
+                if ($existingArg instanceof Expr) {
+                    $options[$argName] = $existingArg;
+                }
+            }
+        }
+
+        $primaryKeyProperty = $node->getProperty('primaryKey');
+        if ($primaryKeyProperty !== null && $primaryKeyProperty->isProtected()) {
+            $primaryKeyValue = $this->getPropertyDefaultValue($primaryKeyProperty);
+            if ($primaryKeyValue instanceof Expr) {
+                $options['key'] = $primaryKeyValue;
+            } else {
+                $primaryKeyProperty = null;
+            }
+        } else {
+            $primaryKeyProperty = null;
+        }
+
+        $keyTypeProperty = $node->getProperty('keyType');
+        if ($keyTypeProperty !== null && $keyTypeProperty->isProtected()) {
+            $keyTypeValue = $this->getPropertyDefaultValue($keyTypeProperty);
+            if ($keyTypeValue instanceof Expr) {
+                $options['keyType'] = $keyTypeValue;
+            } else {
+                $keyTypeProperty = null;
+            }
+        } else {
+            $keyTypeProperty = null;
+        }
+
+        $incrementingProperty = $node->getProperty('incrementing');
+        if ($incrementingProperty !== null && $incrementingProperty->isProtected()) {
+            $incrementingValue = $this->getPropertyDefaultValue($incrementingProperty);
+            if ($incrementingValue instanceof Expr) {
+                $options['incrementing'] = $incrementingValue;
+            } else {
+                $incrementingProperty = null;
+            }
+        } else {
+            $incrementingProperty = null;
+        }
+
+        // If the attribute exists but there are no properties to remove, there is nothing to do
+        if ($hasExistingAttribute && $tableProperty === null && $primaryKeyProperty === null && $keyTypeProperty === null && $incrementingProperty === null) {
+            return null;
+        }
+
+        $attributeGroup = $this->tableAttributeFactory->create($tableValue, $options);
+
+        if ($hasExistingAttribute) {
+            foreach ($node->attrGroups as $key => $attrGroup) {
+                foreach ($attrGroup->attrs as $attr) {
+                    if ($this->isName($attr->name, $tableAttributeClass)) {
+                        $node->attrGroups[$key] = $attributeGroup;
+                        break 2;
+                    }
+                }
+            }
+        } else {
+            $node->attrGroups[] = $attributeGroup;
+        }
+
+        // Remove properties
+        if ($tableProperty !== null) {
+            $this->removeProperty($node, $tableProperty);
+        }
+        if ($primaryKeyProperty !== null) {
+            $this->removeProperty($node, $primaryKeyProperty);
+        }
+        if ($keyTypeProperty !== null) {
+            $this->removeProperty($node, $keyTypeProperty);
+        }
+        if ($incrementingProperty !== null) {
+            $this->removeProperty($node, $incrementingProperty);
+        }
+
+        return $node;
+    }
+
+    private function getPropertyDefaultValue(Property $property): ?Expr
+    {
+        return $property->props[0]->default;
+    }
+
+    private function getExistingAttributeArg(Class_ $class, string $attributeClass, string $argName): ?Expr
+    {
+        foreach ($class->attrGroups as $attrGroup) {
+            foreach ($attrGroup->attrs as $attr) {
+                if ($this->isName($attr->name, $attributeClass)) {
+                    foreach ($attr->args as $arg) {
+                        if ($arg->name !== null && $arg->name->name === $argName) {
+                            return $arg->value;
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private function removeProperty(Class_ $class, Property $property): void
+    {
+        foreach ($class->stmts as $key => $stmt) {
+            if ($stmt === $property) {
+                unset($class->stmts[$key]);
+                break;
+            }
+        }
+    }
+}
