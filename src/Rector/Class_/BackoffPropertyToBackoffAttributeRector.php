b6cf206e3f9df95f9ee340c710321da59577ea20
Peter Fox <peter.fox@peterfox.me>
2026-03-16 15:20:48 +0000

    Adds rules for Laravel Queue Job properties to attributes

---
+++ src/Rector/Class_/BackoffPropertyToBackoffAttributeRector.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RectorLaravel\Rector\Class_;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Attribute;
+use PhpParser\Node\AttributeGroup;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Stmt\Class_;
+use PHPStan\Type\ObjectType;
+use Rector\Php80\NodeAnalyzer\PhpAttributeAnalyzer;
+use RectorLaravel\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \RectorLaravel\Tests\Rector\Class_\BackoffPropertyToBackoffAttributeRector\BackoffPropertyToBackoffAttributeRectorTest
+ */
+final class BackoffPropertyToBackoffAttributeRector extends AbstractRector
+{
+    public function __construct(
+        private readonly PhpAttributeAnalyzer $phpAttributeAnalyzer,
+    ) {}
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Changes the backoff property to use the Backoff attribute',
+            [new CodeSample(
+                <<<'CODE_SAMPLE'
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+final class ProcessPodcast implements ShouldQueue
+{
+    public $backoff = 3;
+}
+CODE_SAMPLE,
+                <<<'CODE_SAMPLE'
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\Attributes\Backoff;
+
+#[Backoff(3)]
+final class ProcessPodcast implements ShouldQueue
+{
+}
+CODE_SAMPLE
+            )]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param  Class_  $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $node->isFinal()) {
+            return null;
+        }
+
+        if (! $this->isObjectType($node, new ObjectType('Illuminate\Contracts\Queue\ShouldQueue'))) {
+            return null;
+        }
+
+        if ($this->phpAttributeAnalyzer->hasPhpAttribute($node, 'Illuminate\Queue\Attributes\Backoff')) {
+            return null;
+        }
+
+        $property = $node->getProperty('backoff');
+        if ($property === null) {
+            return null;
+        }
+
+        if (! $property->isPublic()) {
+            return null;
+        }
+
+        $propertyProperty = $property->props[0];
+        if ($propertyProperty->default === null) {
+            return null;
+        }
+
+        $value = $propertyProperty->default;
+
+        // Add attribute to class
+        $node->attrGroups[] = new AttributeGroup([
+            new Attribute(
+                new FullyQualified('Illuminate\Queue\Attributes\Backoff'),
+                [new Arg($value)]
+            ),
+        ]);
+
+        // Remove property
+        foreach ($node->stmts as $key => $stmt) {
+            if ($stmt === $property) {
+                unset($node->stmts[$key]);
+                break;
+            }
+        }
+
+        return $node;
+    }
+}
