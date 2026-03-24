b6535b3833ea5605a0c4ea7709a3f59795b45a03
Achyut Neupane <30431426+achyutkneupane@users.noreply.github.com>
2026-03-24 21:54:50 +0545

    Add Laravel 13 support to Level Set List (#485)
    * feat: adds Laravel13 in Level Set List
    
    * docs: updates Laravel level set list to support Laravel 13
    
    * test: adds Laravel 13 support in LaravelSetProviderTest
    
    ---------
    
    Co-authored-by: Peter Fox <peter.fox@peterfox.me>

--- src/Set/LaravelLevelSetList.php
+++ src/Set/LaravelLevelSetList.php
@@ -35,4 +35,6 @@ final class LaravelLevelSetList
     final public const string UP_TO_LARAVEL_110 = __DIR__ . '/../../config/sets/level/up-to-laravel-110.php';
 
     final public const string UP_TO_LARAVEL_120 = __DIR__ . '/../../config/sets/level/up-to-laravel-120.php';
+
+    final public const string UP_TO_LARAVEL_130 = __DIR__ . '/../../config/sets/level/up-to-laravel-130.php';
 }
