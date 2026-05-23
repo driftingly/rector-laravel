362e790682db4cf358769fbd7b48e5cd513b32dd
Punyapal Shah <mrpunyapal@gmail.com>
2026-05-23 14:21:39 +0530

    lint

--- config/sets/laravel130-attributes.php
+++ config/sets/laravel130-attributes.php
@@ -3,42 +3,37 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-
 use RectorLaravel\Rector\Class_\AliasesPropertyToAliasesAttributeRector;
-use RectorLaravel\Rector\Class_\CommandHiddenPropertyToHiddenAttributeRector;
-use RectorLaravel\Rector\Class_\DescriptionPropertyToDescriptionAttributeRector;
-use RectorLaravel\Rector\Class_\HelpPropertyToHelpAttributeRector;
-use RectorLaravel\Rector\Class_\SignaturePropertyToSignatureAttributeRector;
-
 use RectorLaravel\Rector\Class_\AppendsPropertyToAppendsAttributeRector;
 use RectorLaravel\Rector\Class_\BackoffPropertyToBackoffAttributeRector;
 use RectorLaravel\Rector\Class_\CollectedByPropertyToCollectedByAttributeRector;
+use RectorLaravel\Rector\Class_\CollectsPropertyToCollectsAttributeRector;
+use RectorLaravel\Rector\Class_\CommandHiddenPropertyToHiddenAttributeRector;
 use RectorLaravel\Rector\Class_\ConnectionPropertyToConnectionAttributeRector;
 use RectorLaravel\Rector\Class_\DateFormatPropertyToDateFormatAttributeRector;
+use RectorLaravel\Rector\Class_\DelayPropertyToDelayAttributeRector;
+use RectorLaravel\Rector\Class_\DeleteWhenMissingModelsPropertyToDeleteWhenMissingModelsAttributeRector;
+use RectorLaravel\Rector\Class_\DescriptionPropertyToDescriptionAttributeRector;
+use RectorLaravel\Rector\Class_\ErrorBagPropertyToErrorBagAttributeRector;
+use RectorLaravel\Rector\Class_\FailOnTimeoutPropertyToFailOnTimeoutAttributeRector;
 use RectorLaravel\Rector\Class_\FillablePropertyToFillableAttributeRector;
 use RectorLaravel\Rector\Class_\GuardedPropertyToGuardedAttributeRector;
+use RectorLaravel\Rector\Class_\HelpPropertyToHelpAttributeRector;
 use RectorLaravel\Rector\Class_\HiddenPropertyToHiddenAttributeRector;
+use RectorLaravel\Rector\Class_\JobConnectionPropertyToJobConnectionAttributeRector;
+use RectorLaravel\Rector\Class_\MaxExceptionsPropertyToMaxExceptionsAttributeRector;
+use RectorLaravel\Rector\Class_\PreserveKeysPropertyToPreserveKeysAttributeRector;
+use RectorLaravel\Rector\Class_\QueuePropertyToQueueAttributeRector;
+use RectorLaravel\Rector\Class_\SignaturePropertyToSignatureAttributeRector;
+use RectorLaravel\Rector\Class_\StopOnFirstFailurePropertyToStopOnFirstFailureAttributeRector;
 use RectorLaravel\Rector\Class_\TablePropertyToTableAttributeRector;
+use RectorLaravel\Rector\Class_\TimeoutPropertyToTimeoutAttributeRector;
 use RectorLaravel\Rector\Class_\TouchesPropertyToTouchesAttributeRector;
+use RectorLaravel\Rector\Class_\TriesPropertyToTriesAttributeRector;
+use RectorLaravel\Rector\Class_\UniqueForPropertyToUniqueForAttributeRector;
 use RectorLaravel\Rector\Class_\VisiblePropertyToVisibleAttributeRector;
 use RectorLaravel\Rector\Class_\WithoutIncrementingPropertyToWithoutIncrementingAttributeRector;
 use RectorLaravel\Rector\Class_\WithoutTimestampsPropertyToWithoutTimestampsAttributeRector;
-
-use RectorLaravel\Rector\Class_\CollectsPropertyToCollectsAttributeRector;
-use RectorLaravel\Rector\Class_\PreserveKeysPropertyToPreserveKeysAttributeRector;
-
-use RectorLaravel\Rector\Class_\ErrorBagPropertyToErrorBagAttributeRector;
-use RectorLaravel\Rector\Class_\StopOnFirstFailurePropertyToStopOnFirstFailureAttributeRector;
-
-use RectorLaravel\Rector\Class_\DelayPropertyToDelayAttributeRector;
-use RectorLaravel\Rector\Class_\DeleteWhenMissingModelsPropertyToDeleteWhenMissingModelsAttributeRector;
-use RectorLaravel\Rector\Class_\FailOnTimeoutPropertyToFailOnTimeoutAttributeRector;
-use RectorLaravel\Rector\Class_\JobConnectionPropertyToJobConnectionAttributeRector;
-use RectorLaravel\Rector\Class_\MaxExceptionsPropertyToMaxExceptionsAttributeRector;
-use RectorLaravel\Rector\Class_\QueuePropertyToQueueAttributeRector;
-use RectorLaravel\Rector\Class_\TimeoutPropertyToTimeoutAttributeRector;
-use RectorLaravel\Rector\Class_\TriesPropertyToTriesAttributeRector;
-use RectorLaravel\Rector\Class_\UniqueForPropertyToUniqueForAttributeRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
