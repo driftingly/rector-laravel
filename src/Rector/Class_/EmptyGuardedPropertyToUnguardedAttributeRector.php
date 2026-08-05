2de5244f4f6796bcf5c2fe5a1b73b7bdb4765e99
COCOUVI <cocouvialexandro74@gmail.com>
2026-08-05 20:17:49 +0100

    make some fix

--- src/Rector/Class_/EmptyGuardedPropertyToUnguardedAttributeRector.php
+++ src/Rector/Class_/EmptyGuardedPropertyToUnguardedAttributeRector.php
@@ -96,6 +96,10 @@ CODE_SAMPLE
             return null;
         }
 
+        if ($this->hasNonEmptyFillableProperty($node)) {
+            return null;
+        }
+
         $node->attrGroups[] = new AttributeGroup([
             new Attribute(new FullyQualified('Illuminate\Database\Eloquent\Attributes\Unguarded')),
         ]);
@@ -108,5 +112,24 @@ CODE_SAMPLE
         }
 
         return $node;
+    }
+
+    private function hasNonEmptyFillableProperty(Class_ $class): bool
+    {
+        $fillableProperty = $class->getProperty('fillable');
+        if ($fillableProperty === null) {
+            return false;
+        }
+
+        if (! $fillableProperty->isProtected()) {
+            return false;
+        }
+
+        $propertyProperty = $fillableProperty->props[0];
+        if ($propertyProperty->default === null || ! $propertyProperty->default instanceof Array_) {
+            return false;
+        }
+
+        return $propertyProperty->default->items !== [];
     }
 }
