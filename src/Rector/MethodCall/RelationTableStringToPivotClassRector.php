588b020ca0e4ba10a2767a64071c1e3e5b5c79cd
Sam Anglin <sam@mizouzie.dev>
2026-08-10 11:00:39 +0300

    Rule to use illuminate/support package and tidy up to satisfy PHPStan

--- src/Rector/MethodCall/RelationTableStringToPivotClassRector.php
+++ src/Rector/MethodCall/RelationTableStringToPivotClassRector.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace RectorLaravel\Rector\MethodCall;
 
+use Illuminate\Support\Str;
 use PhpParser\Node;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Expr\MethodCall;
-use PhpParser\Node\Identifier;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Scalar\String_;
-use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\ReflectionProvider;
 use PHPStan\Type\ObjectType;
 use Rector\Contract\Rector\ConfigurableRectorInterface;
 use RectorLaravel\AbstractRector;
+use RectorLaravel\NodeAnalyzer\ModelAnalyzer;
 use RectorLaravel\Tests\Rector\MethodCall\RelationTableStringToPivotClassRector\RelationTableStringToPivotClassRectorTest;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\ConfiguredCodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -28,25 +28,12 @@ final class RelationTableStringToPivotClassRector extends AbstractRector impleme
 {
     public const string MODEL_NAMESPACES = 'model_namespaces';
 
-    private const string MODEL_CLASS = 'Illuminate\Database\Eloquent\Model';
-
-    private const string TABLE_ATTRIBUTE_CLASS = 'Illuminate\Database\Eloquent\Attributes\Table';
-
-    /**
-     * Position of the $table parameter within each relation method.
-     */
-    private const array TABLE_ARGUMENT_POSITIONS = [
-        'belongsToMany' => 1,
-        'morphToMany' => 2,
-        'morphedByMany' => 2,
-    ];
-
     /**
      * @var list<string>
      */
     private array $modelNamespaces = ['App\Models'];
 
-    public function __construct(private readonly ReflectionProvider $reflectionProvider) {}
+    public function __construct(private readonly ModelAnalyzer $modelAnalyzer, private readonly ReflectionProvider $reflectionProvider) {}
 
     public function getRuleDefinition(): RuleDefinition
     {
@@ -98,13 +85,23 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?MethodCall
     {
-        $tableArgument = $this->getTableArgument($node);
+        $tablePosition = match (true) {
+            $this->isName($node->name, 'belongsToMany') => 1,
+            $this->isName($node->name, 'morphToMany'), $this->isName($node->name, 'morphedByMany') => 2,
+            default => null,
+        };
+
+        if ($tablePosition === null) {
+            return null;
+        }
+
+        $tableArgument = $node->getArg('table', $tablePosition);
 
         if (! $tableArgument instanceof Arg) {
             return null;
         }
 
-        if (! $this->isObjectType($node->var, new ObjectType(self::MODEL_CLASS))) {
+        if (! $this->isObjectType($node->var, new ObjectType('Illuminate\Database\Eloquent\Model'))) {
             return null;
         }
 
@@ -125,30 +122,9 @@ CODE_SAMPLE
         return $node;
     }
 
-    private function getTableArgument(MethodCall $methodCall): ?Arg
-    {
-        foreach (self::TABLE_ARGUMENT_POSITIONS as $method => $position) {
-            if (! $this->isName($methodCall->name, $method)) {
-                continue;
-            }
-
-            foreach ($methodCall->getArgs() as $index => $arg) {
-                if ($arg->name instanceof Identifier) {
-                    if ($this->isName($arg->name, 'table')) {
-                        return $arg;
-                    }
-                } elseif ($index === $position) {
-                    return $arg;
-                }
-            }
-        }
-
-        return null;
-    }
-
     private function resolvePivotClass(string $table): ?string
     {
-        $classNames = array_unique([$this->studly($table), $this->studly($this->singularise($table))]);
+        $classNames = array_unique([Str::studly($table), Str::studly(Str::singular($table))]);
 
         foreach ($this->modelNamespaces as $modelNamespace) {
             foreach ($classNames as $className) {
@@ -160,85 +136,16 @@ CODE_SAMPLE
 
                 $classReflection = $this->reflectionProvider->getClass($pivotClass);
 
-                if (! $classReflection->is(self::MODEL_CLASS)) {
+                if (! $classReflection->is('Illuminate\Database\Eloquent\Model')) {
                     continue;
                 }
 
-                if ($this->resolveTableName($classReflection) === $table) {
+                if ($this->modelAnalyzer->getTable(new ObjectType($pivotClass)) === $table) {
                     return $pivotClass;
                 }
             }
         }
 
         return null;
-    }
-
-    /**
-     * Mirrors Illuminate\Database\Eloquent\Model::getTable().
-     */
-    private function resolveTableName(ClassReflection $classReflection): string
-    {
-        $nativeReflection = $classReflection->getNativeReflection();
-
-        foreach ($nativeReflection->getAttributes(self::TABLE_ATTRIBUTE_CLASS) as $attributeReflection) {
-            $arguments = $attributeReflection->getArguments();
-            $name = $arguments['name'] ?? $arguments[0] ?? null;
-
-            if (is_string($name)) {
-                return $name;
-            }
-        }
-
-        $table = $nativeReflection->getDefaultProperties()['table'] ?? null;
-
-        if (is_string($table)) {
-            return $table;
-        }
-
-        return $this->snake($this->pluralise($nativeReflection->getShortName()));
-    }
-
-    private function studly(string $value): string
-    {
-        return str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $value)));
-    }
-
-    private function snake(string $value): string
-    {
-        return strtolower((string) preg_replace('/(?<!^)[A-Z]/', '_$0', $value));
-    }
-
-    private function singularise(string $word): string
-    {
-        $lower = strtolower($word);
-
-        if (str_ends_with($lower, 'ies')) {
-            return substr($word, 0, -3) . 'y';
-        }
-
-        if (preg_match('/(s|x|z|ch|sh)es$/', $lower) === 1) {
-            return substr($word, 0, -2);
-        }
-
-        if (str_ends_with($lower, 's')) {
-            return substr($word, 0, -1);
-        }
-
-        return $word;
-    }
-
-    private function pluralise(string $word): string
-    {
-        $lower = strtolower($word);
-
-        if (str_ends_with($lower, 'y') && preg_match('/[aeiou]y$/', $lower) === 0) {
-            return substr($word, 0, -1) . 'ies';
-        }
-
-        if (preg_match('/(s|x|z|ch|sh)$/', $lower) === 1) {
-            return $word . 'es';
-        }
-
-        return $word . 's';
     }
 }
