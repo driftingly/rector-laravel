e9a52ce3032524d44ce68bb397cb9c02d669e4c9
Sam Anglin <sam@mizouzie.dev>
2026-08-10 12:29:34 +0300

    Improve stubs

--- stubs/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
+++ stubs/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
@@ -6,4 +6,22 @@ if (class_exists('Illuminate\Database\Eloquent\Relations\BelongsToMany')) {
     return;
 }
 
-class BelongsToMany extends Relation {}
+/**
+ * The pivot model is bound to Model rather than Pivot, as the stubs have no Pivot class.
+ *
+ * @template TRelatedModel of \Illuminate\Database\Eloquent\Model
+ * @template TDeclaringModel of \Illuminate\Database\Eloquent\Model
+ * @template TPivotModel of \Illuminate\Database\Eloquent\Model = \Illuminate\Database\Eloquent\Model
+ *
+ * @extends Relation<TRelatedModel, TDeclaringModel, mixed>
+ */
+class BelongsToMany extends Relation
+{
+    /**
+     * Specify the custom pivot model to use for the relationship.
+     *
+     * @param  class-string  $class
+     * @return $this
+     */
+    public function using($class) {}
+}
--- stubs/Illuminate/Database/Eloquent/Relations/MorphToMany.php
+++ stubs/Illuminate/Database/Eloquent/Relations/MorphToMany.php
@@ -6,4 +6,11 @@ if (class_exists('Illuminate\Database\Eloquent\Relations\MorphToMany')) {
     return;
 }
 
+/**
+ * @template TRelatedModel of \Illuminate\Database\Eloquent\Model
+ * @template TDeclaringModel of \Illuminate\Database\Eloquent\Model
+ * @template TPivotModel of \Illuminate\Database\Eloquent\Model = \Illuminate\Database\Eloquent\Model
+ *
+ * @extends BelongsToMany<TRelatedModel, TDeclaringModel, TPivotModel>
+ */
 class MorphToMany extends BelongsToMany {}
