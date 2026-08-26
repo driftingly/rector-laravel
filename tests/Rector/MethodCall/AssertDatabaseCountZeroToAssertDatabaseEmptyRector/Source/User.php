cba71ad8ec7dad42e0bae1c851603ab482ce60ec
Sam Anglin <sam@mizouzie.dev>
2026-08-26 18:57:50 +0300

    Test and Fixtures

---
+++ tests/Rector/MethodCall/AssertDatabaseCountZeroToAssertDatabaseEmptyRector/Source/User.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace RectorLaravel\Tests\Rector\MethodCall\AssertDatabaseCountZeroToAssertDatabaseEmptyRector\Source;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model {}
