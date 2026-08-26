a0d9137692a392d6556e5541362cb6c62d435a58
Sam Anglin <sam@mizouzie.dev>
2026-08-26 19:40:11 +0300

    Drop source noise

--- tests/Rector/MethodCall/AssertDatabaseCountZeroToAssertDatabaseEmptyRector/Source/User.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace RectorLaravel\Tests\Rector\MethodCall\AssertDatabaseCountZeroToAssertDatabaseEmptyRector\Source;
-
-use Illuminate\Database\Eloquent\Model;
-
-class User extends Model {}
